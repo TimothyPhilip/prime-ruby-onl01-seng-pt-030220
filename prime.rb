e425e91b34a0bbe5f946d178ffc92a5858aecf39
Philippe Timothe <philippetimothe@gmail.com>
2020-03-15 15:06:35 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime?(integer)
   i = 2
   while i < integer
-    return false if integer % i == 0 || integer.negative?()
+    return false if integer <= 2 || integer.negative?()
    i = i + 1
   end
   true
