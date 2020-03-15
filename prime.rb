a6475af52276115581d8b29623af4655f81a004f
Philippe Timothe <philippetimothe@gmail.com>
2020-03-15 19:25:30 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,7 @@ def prime?(integer)
   (2...integer -1).each do |num|
     if integer % num == 0 
       return false 
-    elsif integer < 0
-      return false
+    elsif integer
     end
     
   end
